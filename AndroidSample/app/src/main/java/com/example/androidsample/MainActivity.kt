fced8fce9515f128c75f1fe67490a437d39426d2
khj5014 <64896739+khj5014@users.noreply.github.com>
2021-12-30 15:48:07 +0900

    환율변환

--- AndroidSample/app/src/main/java/com/example/androidsample/MainActivity.kt
+++ AndroidSample/app/src/main/java/com/example/androidsample/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.androidsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun converCurrency(view: View){
+        if (dollarText.text.isNotEmpty()){
+            val dollarValue = dollarText.text.toString().toFloat()
+            val euroValue = dollarValue * 1100f
+            textView.text = euroValue.toString()
+        } else
+            textView.text = getString(R.string.no_value_String)
+    }
+
 }
