df873f1c5bc0eaf402656d78b06a476a9ac6d542
khj5014 <64896739+khj5014@users.noreply.github.com>
2022-01-13 17:01:41 +0900

    gg

---
+++ arSample02/app/src/main/java/com/example/arsample02/ActivityMain.kt
@@ -0,0 +1,46 @@
+package com.example.arsample02
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.core.Anchor
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.BaseArFragment
+import com.google.ar.sceneform.ux.TransformableNode
+
+class ActivityMain : AppCompatActivity() {
+    private lateinit var arFragment: ArFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        arFragment = supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment
+
+        arFragment.setOnTapArPlaneListener(BaseArFragment.OnTapArPlaneListener { hitResult, plane, motionEvent ->
+            var anchor: Anchor = hitResult.createAnchor()
+
+            ModelRenderable.builder()
+                .setSource(this, Uri.parse("andy.sfb"))
+                .build()
+                .thenAccept { addModelToScence(anchor, it) }
+                .exceptionally {
+                    val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+                    builder.setMessage(it.localizedMessage)
+                        .show()
+                    return@exceptionally null
+                }
+        })
+    }
+    private fun addModelToScence(anchor: Anchor, it: ModelRenderable?) {
+        val anchorNode : AnchorNode = AnchorNode(anchor)
+        val transform : TransformableNode = TransformableNode(arFragment.transformationSystem)
+        transform.setParent(anchorNode)
+        transform.renderable = it
+        arFragment.arSceneView.scene.addChild(anchorNode)
+        transform.select()
+
+    }
+}
