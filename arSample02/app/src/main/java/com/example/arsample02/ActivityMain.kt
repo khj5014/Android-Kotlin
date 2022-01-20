19e260f70c54cc03f032cd449169652d47e0437e
khj5014 <64896739+khj5014@users.noreply.github.com>
2022-01-20 17:59:43 +0900

    no dap

--- arSample02/app/src/main/java/com/example/arsample02/ActivityMain.kt
+++ arSample02/app/src/main/java/com/example/arsample02/ActivityMain.kt
@@ -2,8 +2,12 @@ package com.example.arsample02
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.amodtech.ar.lineview.LineViewMainActivity
+import com.example.arsample02.databinding.ActivityMainBinding
 import com.google.ar.core.Anchor
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.rendering.ModelRenderable
@@ -11,19 +15,24 @@ import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.BaseArFragment
 import com.google.ar.sceneform.ux.TransformableNode
 
+
 class ActivityMain : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private lateinit var arFragment: ArFragment
+    private var currentSelectedAnchorNode: AnchorNode? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         arFragment = supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment
 
         arFragment.setOnTapArPlaneListener(BaseArFragment.OnTapArPlaneListener { hitResult, plane, motionEvent ->
             var anchor: Anchor = hitResult.createAnchor()
 
             ModelRenderable.builder()
-                .setSource(this, Uri.parse("10778_Toilet_V2.sfb"))
+                .setSource(this, Uri.parse("andy.sfb"))
                 .build()
                 .thenAccept { addModelToScence(anchor, it) }
                 .exceptionally {
@@ -33,11 +42,19 @@ class ActivityMain : AppCompatActivity() {
                     return@exceptionally null
                 }
         })
+
+        val deleteButton = binding.mybutton
+        deleteButton.setOnClickListener(View.OnClickListener { //Delete the Anchor if it exists
+            Log.d(LineViewMainActivity.TAG, "Deleteing anchor")
+            removeAnchorNode()
+            currentSelectedAnchorNode = null
+
+        })
     }
 
     private fun addModelToScence(anchor: Anchor, it: ModelRenderable?) {
-        val anchorNode : AnchorNode = AnchorNode(anchor)
-        val transform : TransformableNode = TransformableNode(arFragment.transformationSystem)
+        val anchorNode: AnchorNode = AnchorNode(anchor)
+        val transform: TransformableNode = TransformableNode(arFragment.transformationSystem)
         transform.setParent(anchorNode)
         transform.renderable = it
         arFragment.arSceneView.scene.addChild(anchorNode)
@@ -46,9 +63,9 @@ class ActivityMain : AppCompatActivity() {
 
     private fun removeAnchorNode(nodeToRemove: AnchorNode) {
         //Remove an Anchor node
-        arFragment.getArSceneView().getScene().removeChild(nodeToRemove);
-        nodeToRemove.getAnchor()?.detach();
-        nodeToRemove.setParent(null);
+        arFragment.getArSceneView().getScene().removeChild(nodeToRemove)
+        nodeToRemove.getAnchor()?.detach()
+        nodeToRemove.setParent(null)
         nodeToRemove.renderable = null
     }
 }
